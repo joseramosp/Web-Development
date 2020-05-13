7384836d28914cb8b61e13f4acf98e6736c957cd
Jose Ramos <jmrp.04@gmail.com>
2020-05-13 04:59:47 -0400

    Simon Game: My version of the game is already done.

--- (My version) Simon Game Challenge Starting Files/index.js
+++ (My version) Simon Game Challenge Starting Files/index.js
@@ -1,7 +1,10 @@
-var isGameOver = true;
+var gameIsOver = true;
 var buttons = document.querySelectorAll("button");
 
 var sequence = [];
+var index = 0;
+var sequenceIndex = 0;
+var level = 1;
 
 buttons.forEach(function (button){
     pressedAnimation(button);
@@ -13,19 +16,53 @@ function addRandomColor(){
 }
 
 function runSequence(){
-
-    for(var index = 0; index < sequence.length; index++){
-        playSquare(sequence[index]);
+    var index = 0;
+    for(var i = 0; i < sequence.length; i++){
+        var buttons = sequence;
+        setTimeout(function () {
+            playSquare(buttons[index++]);
+        }, ((index++ + 1) * 1000));
     }
+    index = 0;
+    sequenceIndex = 0;
 }
 
 function pressedAnimation(button){
     $(button).on("click", function(){
-        playSquare(button);
+        userSelectedSquare(button);
+    });
+}
+
+function userSelectedSquare(button){
+
+    if(button === sequence[sequenceIndex]){
+        sequenceIndex++;
+        $(button).css("background-color","grey");
+        setTimeout(function () {
+            $(button).css("background-color",button.id);
+        }, 200);
         var audio;
         audio = new Audio("sounds/" + button.id + ".mp3");
         audio.play();
-    });
+        // level++;
+    }
+    else{
+        $("body").css("background-color","red");
+        $("h1").text("Game Over, Press Any Key to Restart");
+        setTimeout(function () {
+            $("body").css("background-color","#0A1F40");
+        }, 1000);
+        var audio;
+        audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+        sequenceIndex = 0;
+        level = 1;
+    }
+    if(sequenceIndex === sequence.length){
+        level++;
+        continueGame();
+        sequenceIndex = 0;
+    }
 }
 
 function playSquare(button){
@@ -33,24 +70,18 @@ function playSquare(button){
     setTimeout(function () {
         $(button).removeClass("pressed");
     }, 200);
+    var audio;
+    audio = new Audio("sounds/" + button.id + ".mp3");
+    audio.play();
 }
 
 $("body").on("keypress", function(){
-    if(isGameOver === true){
-        isGameOver = false;
-        startGame();
-    }
+        sequence = [];
+        continueGame();
 });
 
-function startGame(){
-    var level = 1;
-
+function continueGame(){
     addRandomColor();
-    addRandomColor();
-    addRandomColor();
-    addRandomColor();
-    addRandomColor();
-
     runSequence();
-
+    $("h1").text("Level " + level);
 }
